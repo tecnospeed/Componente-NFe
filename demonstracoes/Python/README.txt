2936413b3fc10b3f1c5d7b917a2e7a8de2f16f7e
Rodrigo Alencar <rodrigo.alencar@tecnospeed.com.br>
2020-03-30 16:57:11 -0300

    Exemplo básico de utilização do spdNFe em Python

---
+++ demonstracoes/Python/README.txt
@@ -0,0 +1,6 @@
+ ==========================================README==========================================
+ - Exemplo básico contendo:
+   * Como instanciar o componente spdNFeX;
+   * Como configurá-lo via LoadConfig;
+   * Como fazer a chamada de do método StatusDoServico.
+ ===========================================================================================
---
+++ demonstracoes/Python/main.py
@@ -0,0 +1,13 @@
+import win32com.client
+
+nfe = win32com.client.Dispatch("NFeX.spdNFeX")
+
+""" Configurar informando o CNPJ da software house"""
+nfe.ConfigurarSoftwareHouse("48446555000198","") 
+
+"""Informar o path completo do arquivo nfeConfig.ini"""
+nfe.LoadConfig(r"C:\demo\nfeconfig.ini")
+
+print("Iniciando consulta de status do servico")
+print(nfe.StatusDoServico())
+print("Finalizando consulta de status do servico")
