dd10de57238cdc4dad700e821f32a3870de5fae4
Gabriel Cunha <105884532+gabrielcunhatecnospeed@users.noreply.github.com>
2024-04-24 09:13:22 -0300

    Update main.py

--- demonstracoes/Python/main.py
+++ demonstracoes/Python/main.py
@@ -3,7 +3,7 @@ import win32com.client
 nfe = win32com.client.Dispatch("NFeX.spdNFeX")
 
 """ Configurar informando o CNPJ da software house"""
-nfe.ConfigurarSoftwareHouse("48446555000198","") 
+nfe.ConfigurarSoftwareHouse("","") 
 
 """Informar o path completo do arquivo nfeConfig.ini"""
 nfe.LoadConfig(r"C:\demo\nfeconfig.ini")
